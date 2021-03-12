7641495b246a8b6df72ca49b9515d45f32affd79
leo1553 <leonardoleal.qm@hotmail.com.br>
2021-03-11 21:09:05 -0300

    Added Electron builder

--- main.js
+++ main.js
@@ -10,7 +10,8 @@ function createWindow() {
         height: 600,
         webPreferences: {
             nodeIntegration: true
-        }
+        },
+        autoHideMenuBar: true
     });
 
     let url = `file://${path.join(__dirname, '/dist/index.html')}`;
